e46050256afaf135875f93e328e3c9dba8a2ad37
Alagu veera siranjivee <163497543+siranjivee123@users.noreply.github.com>
2026-04-03 19:36:00 +0530

    Implement dark mode toggle, form validation, and image zoom
    Added dark mode toggle, form validation, and image zoom functionality.

--- profile.js
+++ profile.js
@@ -1,1 +1,25 @@
+const toggleBtn = document.getElementById("darkModeToggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+
+//  Form Validation :
+document.querySelector("form").addEventListener("submit", function(e) {
+  const name = document.querySelector('input[name="name"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  if (name === "" || email === "") {
+    alert("Please fill all fields!");
+    e.preventDefault();
+  } else {
+    alert("Form submitted successfully ");
+  }
+});
+
+
+//  Image Zoom :
+const img = document.querySelector("img");
+img.addEventListener("click", () => {
+    img.style.transform === "scale(1.5)" ? "scale(1)" : "scale(1.5)";
+});
 
